15c2a6e9b463fbdcfe888133f80d1fe1392afc5d
James W Albersheim <jameswalbersheim@BCSR-10839.local>
2019-02-15 14:11:06 -0700

    finished generics

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -19,12 +19,21 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C: MutableCollection<T>> Collection<T>.partitionTo(left: C,
+                                                           right: C,
+                                                           p: (T) -> Boolean): Pair<C, C> {
+    this.forEach { element ->
+        if (p(element)) left.add(element)
+        else            right.add(element)
+    }
+
+    return Pair(left, right)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
